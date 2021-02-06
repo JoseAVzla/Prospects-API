d49d0819d541a06a654491fd1652df2cb021977e
josevalenzuela <pepe13.2.25@gmail.com>
2021-02-06 16:59:02 -0700

    Adding port info

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import { createConnection } from "typeorm";
 import * as cors from "cors";
 import routes from "./routes/prospectos.routes";
-const port = 3001;
+const port =  process.env.port || 3001;
 
 createConnection()
   .then(() => {
